c75493c4fb5577ca9e05a0cd6e4d033fbb96cad2
Samuel Makinde <makindesamuel1999@gmail.com>
2023-05-01 21:37:07 +0100

    created main page and product items

---
+++ dowellchat/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './main-page/main';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ dowellchat/src/products/ProductsList.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styles from './product.module.css'
+
+export default function Products() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.productlist}>
+            <div className={styles.product}>
+                <button>Workflow AI  <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button>Workflow AI  <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Wifi QR Code 6 <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> User Experience Live <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button>Social Media Automation  <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Living Lab Scales  <span>4</span></button>
+            </div><div className={styles.product}>
+                <button> Logo Scan <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Living Lab Chat  <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Customer Experience <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Living Lab Admin <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Team Management <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Living Lab Monitoring <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Sales Agent <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Permutation Calculator <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> DoWell CSC  <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button> Secure Repositories <span>4</span></button>
+            </div>
+            <div className={styles.product}>
+                <button>  <span>4</span></button>
+            </div>
+        </div>
+    </div>
+  )
+}
