54866d1e759382829491aee96d984c2f59792855
uh-sman <75017192+uh-sman@users.noreply.github.com>
2023-05-10 23:10:11 +0100

    Connecting to the repo and testing connection

---
+++ dowellchat/.history/src/App_20230510230054.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './main-page/main';
+import Products from './products/ProductsList';
+import Navbar from './navbar/navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainPage />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
