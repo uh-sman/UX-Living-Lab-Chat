b22e5e9e0857e86104b7b68b325ad9b341167f65
uh-sman <75017192+uh-sman@users.noreply.github.com>
2023-05-22 15:29:40 +0100

    scroll removed via footer

---
+++ dowellchat/.history/src/container/Chats/message/ScrollBar_20230519113639.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./scrollbar.css";
+import Message from "./Message";
+import ReplyChat from "./ReplyChat";
+const ScrollBar = ({ message }) => {
+  return (
+    <div>
+      <div id="wrapper">
+        <div class="scrollbar" id="style-1">
+          <div class="force-overflow">
+            <Message message={message} />
+            {/* <ReplyChat /> */}
+          </div>
+        </div>
+
+        {/* <div class="scrollbar" id="style-2">
+          <div class="force-overflow"></div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default ScrollBar;
